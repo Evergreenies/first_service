75d3e0a62fcfe5d606cb4a25e735cd7a0da6ccfd
evergreenies <evergreenlief@gmail.com>
2024-02-13 21:09:19 +0530

    Adding http_client

--- routes/first_service.py
+++ routes/first_service.py
@@ -1,5 +1,5 @@
 from twelve_factor_app_framework.bootstrap import app
 from views.first_service import home, demo
 
-app.add_endpoint("/", "home", home)
-app.add_endpoint("/virtual", "demo", demo)
+app.add_endpoint(['GET'], "/", "home", home)
+app.add_endpoint(['GET'], "/virtual", "demo", demo)
